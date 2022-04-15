4b0f36332943fed7207ea396e0ab6186351c569d
TOVTC <veronica.tc.to@outlook.com>
2022-04-15 04:19:45 -0400

    modularized code using local node API to generate HTML file

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = profileDataArr => {
-    //this...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-console.log("==========");
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-//is the same as this...
-profileDataArr.forEach((profileItem) => console.log(profileItem));
-}
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+// const printProfileData = profileDataArr => {
+//     //this...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//     }
+
+// console.log("==========");
+
+// //is the same as this...
+// profileDataArr.forEach((profileItem) => console.log(profileItem));
+// }
+
+// printProfileData(profileDataArgs);
+
+fs.writeFile("./index.html", generatePage(name,github), err => {
+    if (err) throw err;
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
